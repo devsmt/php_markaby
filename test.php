0922cdaa946337e1e4cfd52adea42e444b4c980b
taz <devsmt@gmail.com>
2012-05-31 19:45:49 +0200

    moved test code on a separate file

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+/*
+require 'markaby'
+
+mab = Markaby::Builder.new
+mab.html do
+  head { title "Boats.com"
+  body do
+    h1 "Boats.com has great deals"
+    ul do
+      li "$49 for a canoe"
+      li "$39 for a raft"
+      li "$29 for a huge boot that floats and can fit 5 people"
+    end
+  end
+end
+puts mab.to_s
+*/
+require 'php_markaby.php';
+use \Markaby\TagBuilder as T;
+
+$m = new \Markaby\Builder();
+$m->head( array('title' => "Boats.com") )
+    ->html5(function(){
+      return
+      T::body(function(){
+        return
+        T::h1("Boats.com has great deals").
+        T::ul(function(){
+          return
+          T::li("$49 for a canoe").
+          T::li("$39 for a raft").
+          T::li("$29 for a huge boot that floats and can fit 5 people");
+        });
+      });
+    });
+echo $m;
